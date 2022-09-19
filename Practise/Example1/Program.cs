cfe77fdb76aa5bd4ed0d640240da246d9b407acd
Dmitrii <Dmirii@gmail.com>
2022-09-20 00:07:07 +0300

    Practise Ex1 commit

--- Practise/Example1/Program.cs
+++ Practise/Example1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число А ");
+string inputA = Console.ReadLine();
+int numberA = int.Parse(inputA);
+
+Console.Write("Введите число В ");
+string inputB = Console.ReadLine();
+int numberB = int.Parse(inputB);
+
+
+if (numberB * numberB == numberA) 
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
+
