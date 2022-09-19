8635f10b17ae3b177dbcc0b6355ce81e855714ad
Dmitrii <Dmirii@gmail.com>
2022-09-20 00:14:34 +0300

    3

---
+++ Example4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "dima")
+{
+    Console.WriteLine("You're Superman!");
+}
+else
+{
+    Console.Write ("Hello, ");
+    Console.WriteLine(username);
+}
