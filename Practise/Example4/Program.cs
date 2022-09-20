8b3ebe4d33c13260d281e1b5a31e55e5a2c1c07b
Dmitrii <Dmirii@gmail.com>
2022-09-20 21:17:23 +0300

    Commit practise exp.4

---
+++ Practise/Example4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int n = int.Parse(number);
+
+if ((n >= 100) && (n < 1000)) {
+int ost = n % 10;
+Console.WriteLine(ost);
+}
+else {
+Console.WriteLine("Число не трехзначное!");
+}
