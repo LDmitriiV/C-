a8d0b1fc3d0b7a40647d08519630fb0855401fff
Dmitrii <Dmirii@gmail.com>
2022-09-20 20:00:03 +0300

    Commit practise exp.2

--- Practise/Example2/Program.cs
+++ Practise/Example2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите день недели ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+if(number == 1)
+{
+    Console.WriteLine("понедельник");
+    }
+    else if (number == 2)
+    {
+        Console.WriteLine("вторник");
+        }
+        else if (number == 3)
+        {
+            Console.WriteLine("Среда");
+            }
+            else if (number == 4)
+            {
+                Console.WriteLine("Четверг");
+                }
+                else if (number == 5)
+                {
+                    Console.WriteLine("Пятница");
+                    }
+                    else if (number == 6)
+                    {
+                        Console.WriteLine("Суббота");
+                        }
+                        else if (number == 7)
+                        {
+                            Console.WriteLine("Воскресенье");
+                            }
+else
+{
+Console.WriteLine("нет такого дня");
+}
