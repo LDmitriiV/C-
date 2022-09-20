aaaebf2237bd256ff1ff51ab1e44c519fa5ca516
Dmitrii <Dmirii@gmail.com>
2022-09-20 21:22:47 +0300

    Commit practise exp.4

--- Practise/Example3/Program.cs
+++ Practise/Example3/Program.cs
@@ -1,5 +1,10 @@
 ﻿Console.Write("Введите число N ");
 int number = int.Parse(Console.ReadLine());
 
-for (int count = number * (-1); count <= number; count++)
-Console.Write(count + $" ");
+int count = number * (-1);
+while (count < number)
+{
+    Console.Write(count + $", ");
+    count++;
+}
+Console.Write(count);
