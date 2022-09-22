5c9f1619015386e95daa3d4551215067b585ddfb
Dmitrii <Dmirii@gmail.com>
2022-09-22 22:03:00 +0300

    Practise commit

---
+++ Practise/Practise_1/Example1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число А ");
+string inputA = Console.ReadLine();
+int numberA = int.Parse(inputA);
+
+Console.Write("Введите число В ");
+string inputB = Console.ReadLine();
+int numberB = int.Parse(inputB);
+
+
+if (numberB * numberB == numberA) 
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
+
---
+++ Practise/Practise_1/Example3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число N ");
+int number = int.Parse(Console.ReadLine());
+
+int count = number * (-1);
+while (count < number)
+{
+    Console.Write(count + $", ");
+    count++;
+}
+Console.Write(count);
