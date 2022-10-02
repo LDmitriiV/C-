426c871da168231fd52d60b4dacccc631e8cdd44
Dmitrii <Dmirii@gmail.com>
2022-10-02 14:33:07 +0300

    Lection_3 commit

---
+++ Lection/Lection_3/Example_1/Program.cs
@@ -0,0 +1,54 @@
+﻿int[] arr ={1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i =0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
+
+
+void SelectionSortMin(int[] array)
+{
+ for (int i = 0; i < array.Length - 1; i++)
+ {
+ int minPosition = i;
+ for (int j = i + 1; j < array.Length; j++)
+ {
+ if (array[j] < array[minPosition])
+ {
+ minPosition = j;
+ }
+ }
+ int temporary = array[i];
+ array[i] = array[minPosition];
+ array[minPosition] = temporary;
+ }
+}
+SelectionSortMin(arr);
+PrintArray(arr);
+
+void SelectionSortMax(int[] array)
+{
+ for (int i = 0; i < array.Length - 1; i++)
+ {
+ int maxPosition = i;
+ for (int j = i + 1; j < array.Length; j++)
+ {
+ if (array[j] > array[maxPosition])
+ {
+ maxPosition = j;
+ }
+ }
+ int temporary = array[i];
+ array[i] = array[maxPosition];
+ array[maxPosition] = temporary;
+ }
+}
+SelectionSortMax(arr);
+PrintArray(arr);
