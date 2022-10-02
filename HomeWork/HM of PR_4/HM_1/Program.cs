708fcb2ceb612f2226e2c1ba6200fc0924c2e135
Dmitrii <Dmirii@gmail.com>
2022-10-02 21:40:31 +0300

    HM_4 commit

---
+++ HomeWork/HM of PR_4/HM_1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+Ограничения:
+- Нельзя использовать класс Math
+- Должна быть отдельная функция Power, которая возвразает результат
+- Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода*/
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+int res = Power(a, b);
+
+Console.WriteLine($"{a} в степени {b} = {res}");
+
+int Power(int a, int b)
+{
+    int result = a;
+    for (int i = 1; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+
+
+
---
+++ HomeWork/HM of PR_4/HM_2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+
+Ограничения:
+- Должна быть отдельная функция, которая возвразает результат суммы цифр
+- Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода*/
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int count = n.ToString().Length;
+
+int res = Power(n);
+
+Console.WriteLine($"Сумма цифр в числе {n} = {res}");
+
+int Power(int n)
+{
+    int result = 0;
+    
+    int digit;
+    for (int i = 0; i < count; i++)
+    {
+        digit = n - n % 10;
+        result = result + (n - digit);
+        n = n / 10;
+    }
+    return result;
+}
