ede3a6cc56e0f95b4e815dd6a75232c84312c7b8
Dmitrii <Dmirii@gmail.com>
2022-10-06 20:18:40 +0300

    HomeWork_5 commit

--- Practise/Practise_6/Example_1/Program.cs
+++ Practise/Practise_6/Example_1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Console.Clear();
+ 
+int number = int.Parse(Console.ReadLine()!);
+ 
+Console.WriteLine($"Sum: {RecSum(number)}");
+ 
+ 
+ 
+ 
+int RecSum(int number)
+{
+    if (number > 1)
+    {
+        return number + RecSum(number - 1);
+    }
+    else
+    {
+        return 1;
+    }
+}
--- Practise/Practise_6/Example_2/Program.cs
+++ Practise/Practise_6/Example_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача: Напишите программу, которая принимает на вход число и
+выдаёт количество цифр в числе. Использовать рекурсию
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+Console.Clear();
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(CountNumber(number));
+
+
+int CountNumber(int num)
+{
+if (num / 10 < 1) return 1;
+
+else return 1 + CountNumber(num/10);
+}
