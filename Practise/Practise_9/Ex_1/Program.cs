31a363fe50a3aee00e41d0d377b0c3b913b9ccaa
Dmitrii <Dmirii@gmail.com>
2022-10-18 23:21:33 +0300

    Practise_9 commit

---
+++ Practise/Practise_9/Ex_1/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 70:
+Напишите программу, которая печатает последовательность чисел, 
+где каждое следующее равно сумме двух предыдущих. 
+На вход принимает 3 числа: n1, n2 и N, где n1 n2 - первые числа последовательности,
+N – длина последовательности 
+3 и 4, N = 5 -> 3 4 7 11 18 
+6 и 10, N = 4 -> 6 10 16 26 */
+
+Console.Clear();
+
+Console.Write("Введите n1: ");
+int n1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n2: ");
+int n2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int[] array = new int[N];
+array[0] = n1;
+array[1] = n2;
+
+for (int i = 2; i < N; i++)
+{
+array[i] = array[i - 2] + array[i - 1];
+}
+
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i]} ");
+}
+}
---
+++ Practise/Practise_9/Ex_3/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 72:
+Задан массив, в котором хранится двоичное представление числа.
+Нужно вывести его десятичное представление {0,1,1,1,1} -> 15 */
+
+Console.Clear();
+int[] doubleNum = {0, 1, 1, 1, 1};
+Array.Reverse(doubleNum);
+double result = 0;
+
+for (int i = 0; i < doubleNum.Length; i++)
+{
+    result+= Math.Pow(2, i) * doubleNum[i];
+}
+Console.WriteLine(result);
+
+void PrintArray(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write($"{array[i, j]} ");
+}
+Console.WriteLine("");
+}
+}
